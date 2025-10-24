2dc605ddf7fc4d15f61d38839cb618e8cf0bc89d
Mariana-kiki <acosmariana9@outlook.com>
2025-10-24 13:55:33 -0600

    ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,19 @@
+/*Mariana Gutiérrez Acosta
+clase 3 ejercicio 3*/
+
+#include <stdio.h>
+
+void main()
+{
+ for(int i = 2; i <= 100; i = i + 2)
+   {
+      printf("Num: %d\n", i);
+   }
+}
+o
+#include <stdio.h>
+
+void main()
+{
+  
+}
